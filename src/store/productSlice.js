04458789489b6d1e022998dcc438ceed4bd67709
Abdullah-Tariq-qbatch <abdullah.tariq@qbatch.com>
2023-07-17 13:08:50 +0500

    redux with thunk middleware using given methods

--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -1,4 +1,4 @@
-const { createSlice } = require("@reduxjs/toolkit");
+const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
 
 export const STATUSES = Object.freeze({
   IDLE: "idle",
@@ -13,31 +13,50 @@ const productSlice = createSlice({
     status: STATUSES.IDLE,
   },
   reducers: {
-    setProduccts(state, action) {
-      state.data = action.payload;
-    },
-    setStatus(state, action) {
-      state.status = action.payload;
-    },
+    // setProduccts(state, action) {
+    //   state.data = action.payload;
+    // },
+    // setStatus(state, action) {
+    //   state.status = action.payload;
+    // },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state, action) => {
+        state.status = STATUSES.LOADING;
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.data = action.payload;
+        state.status = STATUSES.IDLE;
+      })
+      .addCase(fetchProducts.rejected, (state, action) => {
+        state.status = STATUSES.ERROR;
+      });
   },
 });
 
 export const { setProduccts, setStatus } = productSlice.actions;
 export default productSlice.reducer;
 
-export function fetchProducts() {
-  return async function fetchProductThunk(dispatch, getState) {
-    // if we need to get something from state in thunk function
-    // const props = getState.cart
-    dispatch(setStatus(STATUSES.LOADING));
-    try {
-      const res = await fetch("https://fakestoreapi.com/products");
-      const data = await res.json();
-      dispatch(setProduccts(data));
-      dispatch(setStatus(STATUSES.IDLE));
-    } catch (err) {
-      console.log(err);
-      dispatch(setStatus(STATUSES.ERROR));
-    }
-  };
-}
+export const fetchProducts = createAsyncThunk("products/fetch", async () => {
+  const res = await fetch("https://fakestoreapi.com/products");
+  const data = await res.json();
+  return data;
+});
+
+// export function fetchProducts() {
+//   return async function fetchProductThunk(dispatch, getState) {
+//     // if we need to get something from state in thunk function
+//     // const props = getState.cart
+//     dispatch(setStatus(STATUSES.LOADING));
+//     try {
+//       const res = await fetch("https://fakestoreapi.com/products");
+//       const data = await res.json();
+//       dispatch(setProduccts(data));
+//       dispatch(setStatus(STATUSES.IDLE));
+//     } catch (err) {
+//       console.log(err);
+//       dispatch(setStatus(STATUSES.ERROR));
+//     }
+//   };
+// }
