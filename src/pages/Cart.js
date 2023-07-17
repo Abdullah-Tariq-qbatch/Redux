fa5feee22d16b7fa0f6d48c5a040b0d812f51cf3
Abdullah-Tariq-qbatch <abdullah.tariq@qbatch.com>
2023-07-17 12:18:20 +0500

    redux use till simple reducers add and remove

---
+++ src/pages/Cart.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { remove } from "../store/cartSlice";
+
+const Cart = () => {
+  const selector = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const handleRemove = (id) => {
+    dispatch(remove(id));
+  };
+  return (
+    <div>
+      <h3>Cart</h3>
+      <div className="cartWrapper">
+        {selector?.map((product) => {
+          return (
+            <div className="cartCard">
+              <img src={product.image} />
+              <h5>{product.title}</h5>
+              <h5>{product.price}</h5>
+              <button className="btn" onClick={() => handleRemove(product.id)}>
+                Remove
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
